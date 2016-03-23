7e51a93c4c243ff42611afd60f798fcbcb1ef747
Kostiantyn Kobylinskyi <kos.koha@gmail.com>
2016-03-23 17:28:41 -0400

    add Project Service implementation to enterprice

---
+++ STS/enterprise/src/main/java/io/khasang/enterprise/service/projectService/ProjectInterface.java
@@ -0,0 +1,9 @@
+package io.khasang.enterprise.service.projectService;
+
+import java.math.BigDecimal;
+
+public interface ProjectInterface {
+    String getDescription();
+    String getTitle();
+    BigDecimal getPrice();
+}
