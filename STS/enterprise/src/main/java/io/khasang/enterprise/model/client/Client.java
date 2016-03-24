42eb18a12574e4e6c38465a8402810d074c2884c
PavelYurtaev <yurtaev.ps@gmail.com>
2016-03-24 16:13:45 +0300

    add method signatures to top-level interfaces, making appropriate changes in hierarchy. Also add masterLogin, masterPassword vars for MasterAccount class.

--- STS/enterprise/src/main/java/io/khasang/enterprise/model/client/Client.java
+++ STS/enterprise/src/main/java/io/khasang/enterprise/model/client/Client.java
@@ -5,5 +5,21 @@ import io.khasang.enterprise.model.UserAccount;
 public abstract class Client extends UserAccount {
     private String companyName;
     private String compamyType;
+
+    public String getCompamyType() {
+        return compamyType;
+    }
+
+    public void setCompamyType(String compamyType) {
+        this.compamyType = compamyType;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
 }
 
--- STS/enterprise/src/main/java/io/khasang/enterprise/model/masteruser/MasterAccount.java
+++ STS/enterprise/src/main/java/io/khasang/enterprise/model/masteruser/MasterAccount.java
@@ -3,9 +3,30 @@ package io.khasang.enterprise.model.masteruser;
 import io.khasang.enterprise.model.Account;
 
 public abstract class MasterAccount implements Account {
+    private String masterLogin;
+    private String masterPassword;
+
     public void requestForDeleteAccount() {
     }
 
     public void requestForUpdateAccount() {
+    }
+
+    @Override
+    public void setLogin(String login) {
+        this.masterLogin = login;
+    }
+
+    @Override
+    public void setPassword(String password) {
+        this.masterPassword = password;
+    }
+
+    public String getMasterLogin() {
+        return masterLogin;
+    }
+
+    public String getMasterPassword() {
+        return masterPassword;
     }
 }
