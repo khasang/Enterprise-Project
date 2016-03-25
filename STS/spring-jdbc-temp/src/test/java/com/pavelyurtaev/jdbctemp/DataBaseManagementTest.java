52762d52bc26bd43db48b8e8d871d7b2f3161913
PavelYurtaev <yurtaev.ps@gmail.com>
2016-03-25 14:45:35 +0300

    add spring-test dependency for testing spring-jdbc queries

--- STS/spring-jdbc-temp/src/test/java/com/pavelyurtaev/jdbctemp/DataBaseManagementTest.java
+++ STS/spring-jdbc-temp/src/test/java/com/pavelyurtaev/jdbctemp/DataBaseManagementTest.java
@@ -1,4 +1,30 @@
 package com.pavelyurtaev.jdbctemp;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration({"file:web/WEB-INF/dispatcher-servlet.xml"})
 public class DataBaseManagementTest {
+
+    @Resource
+    DataBaseManagement manager;
+
+    @Test
+    public void notNullTest() {
+        Assert.assertNotNull(manager);
+    }
+
+    @Test
+    public void createTableTest() {
+        String massage = manager.createTable();
+        Assert.assertEquals("Table successfully created", massage);
+    }
 }
