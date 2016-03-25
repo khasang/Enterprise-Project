005656c57a7d35af47777ac2427d18d20a511d39
Max Gradus <gradus182@gmail.com>
2016-03-25 09:49:13 +0300

    gitignore change

--- STS/spring-jdbc-example/classes/artifacts/spring_jdbc_example_Web_exploded/WEB-INF/views/users.jsp
+++
@@ -1,9 +0,0 @@
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-<head>
-    <title>All Users</title>
-</head>
-<body>
-    <p>${users}</p>
-</body>
-</html>
--- STS/spring-jdbc-example/src/test/java/com/maxdegree/jdbcexample/MainTest.java
+++ STS/spring-jdbc-example/src/test/java/com/maxdegree/jdbcexample/MainTest.java
@@ -4,10 +4,10 @@ import org.junit.Test;
 import org.junit.Assert;
 
 public class MainTest {
-    @Test
-    public void createTableUsersTest(){
-        JDBCManager manager = new JDBCManager();
-        String massage = manager.createTableUsers();
-        Assert.assertEquals("table users created", massage);
-    }
+//    @Test
+//    public void createTableUsersTest(){
+//        JDBCManager manager = new JDBCManager();
+//        String massage = manager.createTableUsers();
+//        Assert.assertEquals("table users created", massage);
+//    }
 }
