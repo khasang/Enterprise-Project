15ca2c9fc0a1cea295d5e8b56774dbe044f959ec
PavelYurtaev <yurtaev.ps@gmail.com>
2016-04-01 02:17:44 +0300

    add completely new model structure (enums not persist in DB  yet)

---
+++ STS/enterprise/src/main/java/io/khasang/enterprise/model/newmodel/Features.java
@@ -0,0 +1,16 @@
+package io.khasang.enterprise.model.newmodel;
+
+public enum Features {
+    PHOTOGALLERY,
+    ONLINEPAYMENTS,
+    HEADLINENEWS,
+    AUDIOVIDEMANAGER,
+    WEBSITESTATISTIC,
+    VOTECASER,
+    PASSWORDGATE,
+    LIVECHAT,
+    EMAILFORMS;
+
+    Features() {
+    }
+}
