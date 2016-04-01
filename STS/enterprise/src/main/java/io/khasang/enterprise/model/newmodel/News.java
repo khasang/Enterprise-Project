0ed0f205a9a5e9e25329cb841232f6c878a74766
PavelYurtaev <yurtaev.ps@gmail.com>
2016-04-01 13:19:10 +0300

    edit news entity

--- STS/enterprise/src/main/java/io/khasang/enterprise/model/newmodel/News.java
+++ STS/enterprise/src/main/java/io/khasang/enterprise/model/newmodel/News.java
@@ -1,4 +1,13 @@
 package io.khasang.enterprise.model.newmodel;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class News {
+
+    @Id
+    @GeneratedValue
+    private int id;
 }
