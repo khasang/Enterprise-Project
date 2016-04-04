d8aa21d5078478a1cc63770a3f91b873b8334b09
PavelYurtaev <yurtaev.ps@gmail.com>
2016-04-04 23:00:46 +0300

    implement user roles entities

--- STS/enterprise/src/main/java/io/khasang/enterprise/model/ClientRole.java
+++ STS/enterprise/src/main/java/io/khasang/enterprise/model/ClientRole.java
@@ -1,4 +1,42 @@
 package io.khasang.enterprise.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "client_role")
 public class ClientRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    private Client client;
+
+
+    private String role;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
