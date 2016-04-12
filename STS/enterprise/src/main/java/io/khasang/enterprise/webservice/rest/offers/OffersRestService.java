b8c01b11a5d53c97bc7124b30d5a9a42b6134322
PavelYurtaev <yurtaev.ps@gmail.com>
2016-04-12 23:06:56 +0300

    move Region.java to webservice package

--- STS/enterprise/src/main/java/io/khasang/enterprise/webservice/rest/offers/OffersRestService.java
+++ STS/enterprise/src/main/java/io/khasang/enterprise/webservice/rest/offers/OffersRestService.java
@@ -1,7 +1,5 @@
 package io.khasang.enterprise.webservice.rest.offers;
 
-import io.khasang.enterprise.model.Region;
-
 import javax.annotation.PostConstruct;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
