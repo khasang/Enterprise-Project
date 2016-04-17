5ac316618f7e1754e2060a0427f9641231326785
PavelYurtaev <yurtaev.ps@gmail.com>
2016-04-17 16:10:46 +0300

    start implement ProjectTrackingService, add ability to review all projects and its orders in Personal Area of employee. add projectOrders.jsp

--- STS/enterprise/src/main/java/io/khasang/enterprise/controller/employee/EmployeeController.java
+++ STS/enterprise/src/main/java/io/khasang/enterprise/controller/employee/EmployeeController.java
@@ -1,17 +1,24 @@
 package io.khasang.enterprise.controller.employee;
 
+import io.khasang.enterprise.model.Project;
+import io.khasang.enterprise.service.ProjectTrackingService;
 import io.khasang.enterprise.webservice.exchangerates.Rates;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/employee")
 public class EmployeeController {
     @Autowired
     private Rates rates;
+    @Autowired
+    private ProjectTrackingService projectTrackingService;
 
     @RequestMapping(value = "/account", method = RequestMethod.GET)
     public String employeeHome(Model model) {
@@ -21,4 +28,18 @@ public class EmployeeController {
         return "employee/account";
     }
 
+    @RequestMapping(value = "/all-projects", method = RequestMethod.GET)
+    public String getProject(Model model) {
+        List<Project> projects = projectTrackingService.getAllProject();
+        model.addAttribute("allProjects", projects);
+        return "employee/projects";
+    }
+
+    @RequestMapping(value = "/all-projects/{projectId}/orders", method = RequestMethod.GET)
+    public String getProjectOrders(@PathVariable("projectId") Integer projectId, Model model) {
+        projectTrackingService.getProjectOrders(projectId);
+        model.addAttribute("ordersOfProject", projectTrackingService.getProjectOrders(projectId));
+        model.addAttribute("project", projectTrackingService.getProjectById(projectId));
+        return "employee/projectOrders";
+    }
 }
