2bf63161a6424bcfffd9ce23196a9d7e074afbe5
Гарбуз Максим Сергеевич <SBT-Garbuz-MS@sigma.sbrf.ru>
2016-10-21 13:17:25 +0300

    test commit

--- STS/enterprise/src/main/java/io/khasang/enterprise/controller/AppController.java
+++ STS/enterprise/src/main/java/io/khasang/enterprise/controller/AppController.java
@@ -22,6 +22,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+//test commit
 @Controller
 public class AppController {
     @Autowired
